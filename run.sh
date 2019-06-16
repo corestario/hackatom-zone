2dc6b700dd98cd894f4e457d765acfa439c611a3
KRogLA <i@krogla.me>
2019-06-16 18:47:02 +0200

    fix genesis users non-interactive creation

--- run.sh
+++ run.sh
@@ -12,16 +12,16 @@ nftd init hackNFT --chain-id NFTChain
 echo "Adding keys..."
 
 echo "Adding genesis account..."
-hhcli keys add validator1 --recover <<< "12345678
+nftcli keys add validator1 --recover <<< "12345678
 base figure planet hazard sail easily honey advance tuition grab across unveil random kiss fence connect disagree evil recall latin cause brisk soft lunch
 "
 
-hhcli keys add alice --recover <<< "12345678
+nftcli keys add alice --recover <<< "12345678
 actor barely wait patrol moral amateur hole clerk misery truly salad wonder artefact orchard grit check abandon drip avoid shaft dirt thought melody drip
 "
 
-hhd add-genesis-account $(hhcli keys show validator1 -a) 1000token,100000000stake
-hhd add-genesis-account $(hhcli keys show alice -a) 1000token
+nftd add-genesis-account $(nftcli keys show validator1 -a) 1000token,100000000stake
+nftd add-genesis-account $(nftcli keys show alice -a) 1000token
 
 echo "Configuring..."
 nftcli config chain-id NFTChain
@@ -29,7 +29,7 @@ nftcli config output json
 nftcli config indent true
 nftcli config trust-node true
 
-nftd gentx --name validator1
+nftd gentx --name validator1 <<< "12345678"
 nftd collect-gentxs
 nftd validate-genesis
 
