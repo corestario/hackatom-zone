c611622a0bfa162d4fa03a97a138167d839122b4
Evgeny Danienko <6655321@bk.ru>
2019-06-16 18:33:23 +0300

    add users

--- run.sh
+++ run.sh
@@ -10,10 +10,18 @@ echo "Initialization..."
 nftd init hackNFT --chain-id NFTChain
 
 echo "Adding keys..."
-nftcli keys add validator1
 
 echo "Adding genesis account..."
-nftd add-genesis-account $(nftcli keys show validator1 -a) 1000token,100000000stake
+hhcli keys add validator1 --recover <<< "12345678
+base figure planet hazard sail easily honey advance tuition grab across unveil random kiss fence connect disagree evil recall latin cause brisk soft lunch
+"
+
+hhcli keys add alice --recover <<< "12345678
+actor barely wait patrol moral amateur hole clerk misery truly salad wonder artefact orchard grit check abandon drip avoid shaft dirt thought melody drip
+"
+
+hhd add-genesis-account $(hhcli keys show validator1 -a) 1000token,100000000stake
+hhd add-genesis-account $(hhcli keys show alice -a) 1000token
 
 echo "Configuring..."
 nftcli config chain-id NFTChain
