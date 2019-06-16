1717ef0491c7ba4c81a379404a8d59f2c7f071ab
b00ris <b00ris@mail.ru>
2019-06-16 19:48:49 +0300

    fix run.sh

--- run.sh
+++ run.sh
@@ -12,17 +12,17 @@ nftd init hackNFT --chain-id NFTChain
 echo "Adding keys..."
 
 echo "Adding genesis account..."
-hhcli keys add validator1 --recover <<< "12345678
+nftcli keys add validator1 --recover <<< "12345678
 base figure planet hazard sail easily honey advance tuition grab across unveil random kiss fence connect disagree evil recall latin cause brisk soft lunch
 "
 
-hhcli keys add alice --recover <<< "12345678
+nftcli keys add alice --recover <<< "12345678
 actor barely wait patrol moral amateur hole clerk misery truly salad wonder artefact orchard grit check abandon drip avoid shaft dirt thought melody drip
 "
 
-hhd add-genesis-account $(hhcli keys show validator1 -a) 1000token,100000000stake
-hhd add-genesis-account $(hhcli keys show alice -a) 1000token
-
+nftd add-genesis-account $(hhcli keys show validator1 -a) 1000token,100000000stake
+nftd add-genesis-account $(hhcli keys show alice -a) 1000token
+cp
 echo "Configuring..."
 nftcli config chain-id NFTChain
 nftcli config output json
